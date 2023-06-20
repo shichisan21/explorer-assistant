6bf3a31be8535da29c0d1c4caf339af50c1eb3cd
shichisan21 <photograph.by@gmail.com>
2023-06-21 01:11:50 +0900

    mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,23 @@
+// src/App.tsx
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import About from "./About";
+import Welcome from "./mobile/Welcome";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <Router>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <Link to='/about'>About</Link>
+      <Link to='/mobile/Welcome'>Welcome</Link>
       <Routes>
-        <Route path='/about' Component={About} />
+        <Route path='/about' element={<About />} />
+        <Route path='/mobile/Welcome' element={<Welcome />} />
       </Routes>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
     </Router>
   );
 }
