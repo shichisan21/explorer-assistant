52ccf0c29d2784beb872f427ab091d520509d7bd
shichisan21 <photograph.by@gmail.com>
2023-06-24 14:40:07 +0900

    アップロード

--- src/About.tsx
+++ src/About.tsx
@@ -1,16 +1,59 @@
-import React from "react";
-import Typography from "@mui/material/Typography";
+/**
+ * File Description Aboutページ
+ */
 
-const About: React.FC = () => {
+/**
+ * Import
+ */
+import React, { useState } from "react";
+import Typography from "@mui/material/Typography";
+import axios from "axios";
+import { Box, Button } from "@mui/material";
+
+/**
+ * Interface
+ */
+interface AboutProps {
+  url: string;
+}
+
+interface ResponseData {
+  Hello: string;
+}
+
+/**
+ * Type
+ */
+
+/**
+ * Program
+ */
+
+const About = ({ url }: AboutProps) => {
+  const [data, setData] = useState<ResponseData | null>(null);
+
+  const GetData = () => {
+    axios.get(url).then((res) => {
+      setData(res.data);
+    });
+  };
   return (
-    <div>
+    <Box>
       <Typography variant='h4' component='div' gutterBottom>
         About
       </Typography>
       <Typography variant='body1' gutterBottom>
         Welcome to the About page.
       </Typography>
-    </div>
+      <Box>
+        <Box>This is Axios Test</Box>
+        {data ? (
+          <Box>{data.Hello}</Box>
+        ) : (
+          <Button onClick={GetData}>データを取得</Button>
+        )}
+      </Box>
+    </Box>
   );
 };
 
--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,13 @@ import Welcome from "./mobile/Welcome";
 import ChatRoom from "./mobile/ChatRoom";
 
 function App() {
+  const url = "https://01-api.vercel.app/";
   return (
     <Router>
       <Link to='/about'>About</Link>
       <Link to='/mobile/Welcome'>Welcome</Link>
       <Routes>
-        <Route path='/about' element={<About />} />
+        <Route path='/about' element={<About url={url} />} />
         <Route path='/mobile/Welcome' element={<Welcome />} />
         <Route path='/mobile/Chatroom' element={<ChatRoom />} />
       </Routes>
