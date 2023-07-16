907ee5b044bd8295da6761b4025aff326a70f4e5
shichisan21 <photograph.by@gmail.com>
2023-07-16 16:50:42 +0900

    Post内容の変更

--- src/component/TalkPost.tsx
+++ src/component/TalkPost.tsx
@@ -1,32 +1,40 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Box, Button } from "@mui/material";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
 interface Item {
-  name: string;
+  startDate: string;
+  endDate: string;
 }
 
 const App: React.FC = () => {
+  const [selectedStartDate, setSelectedStartDate] = useState<Date | null>(
+    new Date()
+  );
+  const [selectedEndDate, setSelectedEndDate] = useState<Date | null>(
+    new Date(new Date().setMonth(new Date().getMonth() + 1))
+  );
   const [response, setResponse] = useState<any>(null);
-  const itemToSend: Item = { name: new Date().toISOString() };
 
-  // useEffect(() => {
-  //   const sendRequest = async () => {
-  //     try {
-  //       const res = await axios.post(
-  //         "http://127.0.0.1:8000/put_item/",
-  //         itemToSend
-  //       );
-  //       setResponse(res.data);
-  //     } catch (error: any) {
-  //       console.error("Error during request", error);
-  //       setResponse(error.message);
-  //     }
-  //   };
-  //   sendRequest();
-  // }, []);
+  const handleStartDateChange = (date: Date | null) => {
+    setSelectedStartDate(date);
+  };
+
+  const handleEndDateChange = (date: Date | null) => {
+    setSelectedEndDate(date);
+  };
 
   const onClickHandler = async (): Promise<void> => {
+    if (!selectedStartDate || !selectedEndDate) return;
+
+    const itemToSend: Item = {
+      startDate: selectedStartDate.toISOString(),
+      endDate: selectedEndDate.toISOString(),
+    };
+
     try {
       const res = await axios.post(
         "http://127.0.0.1:8000/put_item/",
@@ -43,6 +51,18 @@ const App: React.FC = () => {
     <Box>
       <h1>Response:</h1>
       <pre>{JSON.stringify(response, null, 2)}</pre>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <DatePicker
+          label='Start Date'
+          value={selectedStartDate}
+          onChange={handleStartDateChange}
+        />
+        <DatePicker
+          label='End Date'
+          value={selectedEndDate}
+          onChange={handleEndDateChange}
+        />{" "}
+      </LocalizationProvider>
       <Button onClick={onClickHandler}>Click!!</Button>
     </Box>
   );
