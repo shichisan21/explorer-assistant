de12ae2b409d58cb55de00d5f5a5f358d14013a7
shichisan21 <photograph.by@gmail.com>
2023-07-28 21:33:18 +0900

    TOPページ改造

--- src/component/HomePage.tsx
+++ src/component/HomePage.tsx
@@ -1,13 +1,47 @@
-import { animated, useSpring } from "react-spring";
+import { animated, useSpring, useTransition } from "react-spring";
+import { useState, useEffect } from "react";
 
 const HomePage: React.FC = () => {
-  const fade = useSpring({
-    from: { opacity: 0 },
-    to: { opacity: 1 },
-    delay: 500,
+  const [items, setItems] = useState<string[]>([]);
+  const transitions = useTransition(items, {
+    from: { opacity: 0, transform: "scale(0.5)" },
+    enter: { opacity: 1, transform: "scale(1)" },
+    leave: { opacity: 0, transform: "scale(0.5)" },
+    delay: 200,
   });
 
-  return <animated.h1 style={fade}>Welcome to the Home Page!</animated.h1>;
+  useEffect(() => {
+    const words = "Welcome to the Home Page!".split(" ");
+    const timer = setInterval(() => {
+      if (words.length > 0) {
+        setItems((oldItems) => [...oldItems, words.shift()!]);
+      }
+    }, 800);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+        background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+        fontFamily: "Helvetica",
+      }}
+    >
+      {transitions((style, item) => (
+        <animated.h1 style={{ ...style, fontSize: "2em", color: "#FFF" }}>
+          {item}
+        </animated.h1>
+      ))}
+    </div>
+  );
 };
 
 export default HomePage;
