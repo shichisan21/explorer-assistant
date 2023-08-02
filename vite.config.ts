ba3d528a79f7e9158c99d1d1c272ff28d657ac6a
shichisan21 <photograph.by@gmail.com>
2023-08-02 23:35:08 +0900

    Vite設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+// 環境変数の値をコンソールに表示
+console.log("VITE_WWW value is:", process.env.VITE_WWW);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
     global: "window",
+    WWW: JSON.stringify(process.env.VITE_WWW), // 追加
   },
 });
