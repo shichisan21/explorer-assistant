9ca5a9fd0efb7321425aadfef21d264bb806acac
shichisan21 <photograph.by@gmail.com>
2023-10-20 18:11:19 +0900

    セレクトナンバー表示

--- src/component/Language.tsx
+++ src/component/Language.tsx
@@ -2,15 +2,17 @@ import React from "react";
 import LanguageSelector from "./SelectLanguage"; // あなたのファイル構成に合わせてパスを変更してください。
 
 const Language: React.FC = () => {
-  const handleLanguageChange = (langCode: number) => {
+  const handleLanguageChange = (langCode: number): number => {
     // ここでlangCodeを使用してAPIを呼び出すなどの処理を行います。
     console.log(`Selected Language Code: ${langCode}`);
+    return langCode;
   };
 
   return (
     <div>
       <h1>Language Selector App</h1>
       <LanguageSelector onLanguageChange={handleLanguageChange} />
+
       {/* 必要に応じて他のコンポーネントや要素を追加します */}
     </div>
   );
--- src/component/SelectLanguage.tsx
+++ src/component/SelectLanguage.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import { Select, MenuItem, SelectChangeEvent } from "@mui/material";
+import { Select, MenuItem, SelectChangeEvent, Typography } from "@mui/material";
 
 interface LanguageSelectorProps {
-  onLanguageChange: (langCode: number) => void;
+  onLanguageChange: (langCode: number) => number;
 }
 
 interface LanguageMappingType {
@@ -18,18 +18,22 @@ const LanguageSelector: React.FC<LanguageSelectorProps> = ({
   onLanguageChange,
 }) => {
   const [selectedLanguage, setSelectedLanguage] = useState<string>("ja");
+  const [langNumber, setLangNumber] = useState<number>(1);
 
   const handleLanguageChange = (event: SelectChangeEvent) => {
     const lang = event.target.value as string;
     setSelectedLanguage(lang);
-    onLanguageChange(reverseMapping[lang]);
+    setLangNumber(onLanguageChange(reverseMapping[lang]));
   };
 
   return (
-    <Select value={selectedLanguage} onChange={handleLanguageChange}>
-      <MenuItem value='ja'>Japanese</MenuItem>
-      <MenuItem value='en'>English</MenuItem>
-    </Select>
+    <>
+      <Select value={selectedLanguage} onChange={handleLanguageChange}>
+        <MenuItem value='ja'>Japanese</MenuItem>
+        <MenuItem value='en'>English</MenuItem>
+      </Select>
+      <Typography>LangNumber: {langNumber}</Typography>
+    </>
   );
 };
 
