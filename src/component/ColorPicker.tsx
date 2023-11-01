8a46ed1fe44c04b1921efb113eb0a89dec3dac70
shichisan21 <photograph.by@gmail.com>
2023-11-01 22:58:20 +0900

    カラーピッカー変更

--- src/component/ColorPicker.tsx
+++ src/component/ColorPicker.tsx
@@ -1,20 +1,11 @@
 // App.tsx or any other component
 
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import { Box, Typography } from "@mui/material";
 import ColorPickerModal from "./ColorPickerModal";
 
-const ColorPicker: React.FC = () => {
+const ColorPickerComponent: React.FC = () => {
   const [color, setColor] = useState<string>("#000000");
-  const [isPickerOpen, setPickerOpen] = useState<boolean>(false);
-  const colorBoxRef = useRef<HTMLDivElement | null>(null);
-
-  const handleColorSelect = (selectedColor?: string) => {
-    if (selectedColor) {
-      setColor(selectedColor);
-    }
-    setPickerOpen(false);
-  };
 
   return (
     <Box
@@ -25,27 +16,70 @@ const ColorPicker: React.FC = () => {
       padding={4}
     >
       <Box
-        ref={colorBoxRef}
         component='button'
         width={50}
         height={50}
         bgcolor={color}
-        onClick={() => setPickerOpen(true)}
         sx={{ border: "none", cursor: "pointer" }}
       ></Box>
       <Box marginTop={2}>
         <Typography variant='h6'>Selected Color: {color}</Typography>
       </Box>
-      {colorBoxRef.current && (
-        <ColorPickerModal
-          open={isPickerOpen}
-          initialColor={color}
-          onClose={handleColorSelect}
-          anchorEl={colorBoxRef.current}
-        />
-      )}
+      <ColorPickerModal initialColor={color} onColorChange={setColor} />
     </Box>
   );
 };
 
-export default ColorPicker;
+export default ColorPickerComponent;
+
+// // App.tsx or any other component
+
+// import React, { useState, useRef } from "react";
+// import { Box, Typography } from "@mui/material";
+// import ColorPickerModal from "./ColorPickerModal";
+
+// const ColorPicker: React.FC = () => {
+//   const [color, setColor] = useState<string>("#000000");
+//   const [isPickerOpen, setPickerOpen] = useState<boolean>(false);
+//   const colorBoxRef = useRef<HTMLDivElement | null>(null);
+
+//   const handleColorSelect = (selectedColor?: string) => {
+//     if (selectedColor) {
+//       setColor(selectedColor);
+//     }
+//     setPickerOpen(false);
+//   };
+
+//   return (
+//     <Box
+//       display='flex'
+//       flexDirection='column'
+//       alignItems='center'
+//       justifyContent='center'
+//       padding={4}
+//     >
+//       <Box
+//         ref={colorBoxRef}
+//         component='button'
+//         width={50}
+//         height={50}
+//         bgcolor={color}
+//         onClick={() => setPickerOpen(true)}
+//         sx={{ border: "none", cursor: "pointer" }}
+//       ></Box>
+//       <Box marginTop={2}>
+//         <Typography variant='h6'>Selected Color: {color}</Typography>
+//       </Box>
+//       {colorBoxRef.current && (
+//         <ColorPickerModal
+//           open={isPickerOpen}
+//           initialColor={color}
+//           onClose={handleColorSelect}
+//           anchorEl={colorBoxRef.current}
+//         />
+//       )}
+//     </Box>
+//   );
+// };
+
+// export default ColorPicker;
--- src/component/ColorPickerModal.tsx
+++ src/component/ColorPickerModal.tsx
@@ -1,45 +1,29 @@
-// ColorPickerModal.tsx
+// ColorPicker.tsx
 
-import React, { useState, useEffect } from "react";
-import { BlockPicker, SketchPicker } from "react-color";
-import {
-  Modal,
-  Box,
-  Button,
-  Grid,
-  useTheme,
-  useMediaQuery,
-} from "@mui/material";
+import React, { useState, useRef, useEffect } from "react";
+import { SketchPicker } from "react-color";
+import { Box, Button, Grid, useMediaQuery, useTheme } from "@mui/material";
 
-interface ColorPickerModalProps {
-  open: boolean;
-  initialColor?: string;
-  onClose: (color?: string) => void;
-  anchorEl: HTMLElement;
+interface ColorPickerProps {
+  initialColor: string;
+  onColorChange: (color: string) => void;
 }
-
-const ColorPickerModal: React.FC<ColorPickerModalProps> = ({
-  open,
-  initialColor = "#000000",
-  onClose,
-  anchorEl,
+const ColorPickerModal: React.FC<ColorPickerProps> = ({
+  initialColor,
+  onColorChange,
 }) => {
   const [favorites, setFavorites] = useState<string[]>([]);
   const [currentColor, setCurrentColor] = useState<string>(initialColor);
-  const [position, setPosition] = useState({ top: 0, left: 0 });
+  const colorPickerRef = useRef<HTMLDivElement | null>(null);
 
   const theme = useTheme();
   const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
 
   useEffect(() => {
-    if (anchorEl) {
-      const rect = anchorEl.getBoundingClientRect();
-      setPosition({
-        top: rect.bottom + window.scrollY,
-        left: rect.left + window.scrollX,
-      });
+    if (currentColor !== initialColor) {
+      onColorChange(currentColor);
     }
-  }, [anchorEl]);
+  }, [currentColor, initialColor, onColorChange]);
 
   const handleColorChange = (colorResult: any) => {
     setCurrentColor(colorResult.hex);
@@ -53,50 +37,136 @@ const ColorPickerModal: React.FC<ColorPickerModalProps> = ({
 
   const handleFavoriteClick = (color: string) => {
     setCurrentColor(color);
-    onClose(color);
   };
 
   return (
-    <Modal open={open} onClose={() => onClose()}>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          position: "absolute",
-          top: `${position.top}px`,
-          left: `${position.left}px`,
-          bgcolor: "white",
-          padding: 4,
-          borderRadius: 2,
-          width: fullScreen ? "90%" : 300,
-        }}
-      >
-        <SketchPicker color={currentColor} onChange={handleColorChange} />
-        <Button onClick={addFavorite} sx={{ marginTop: 2 }}>
-          Add to Favorites
-        </Button>
-        <Box sx={{ marginTop: 2 }}>
-          <Grid container spacing={1}>
-            {favorites.map((color) => (
-              <Grid item key={color}>
-                <Box
-                  sx={{
-                    width: 30,
-                    height: 30,
-                    bgcolor: color,
-                    cursor: "pointer",
-                  }}
-                  onClick={() => handleFavoriteClick(color)}
-                />
-              </Grid>
-            ))}
-          </Grid>
-        </Box>
+    <Box ref={colorPickerRef} sx={{ width: fullScreen ? "90%" : 300 }}>
+      <SketchPicker color={currentColor} onChange={handleColorChange} />
+      <Button onClick={addFavorite} sx={{ marginTop: 2 }}>
+        Add to Favorites
+      </Button>
+      <Box sx={{ marginTop: 2 }}>
+        <Grid container spacing={1}>
+          {favorites.map((color) => (
+            <Grid item key={color}>
+              <Box
+                sx={{
+                  width: 30,
+                  height: 30,
+                  bgcolor: color,
+                  cursor: "pointer",
+                }}
+                onClick={() => handleFavoriteClick(color)}
+              />
+            </Grid>
+          ))}
+        </Grid>
       </Box>
-    </Modal>
+    </Box>
   );
 };
 
 export default ColorPickerModal;
+
+// // ColorPickerModal.tsx
+
+// import React, { useState, useEffect } from "react";
+// import { BlockPicker, SketchPicker } from "react-color";
+// import {
+//   Modal,
+//   Box,
+//   Button,
+//   Grid,
+//   useTheme,
+//   useMediaQuery,
+// } from "@mui/material";
+
+// interface ColorPickerModalProps {
+//   open: boolean;
+//   initialColor?: string;
+//   onClose: (color?: string) => void;
+//   anchorEl: HTMLElement;
+// }
+
+// const ColorPickerModal: React.FC<ColorPickerModalProps> = ({
+//   open,
+//   initialColor = "#000000",
+//   onClose,
+//   anchorEl,
+// }) => {
+//   const [favorites, setFavorites] = useState<string[]>([]);
+//   const [currentColor, setCurrentColor] = useState<string>(initialColor);
+//   const [position, setPosition] = useState({ top: 0, left: 0 });
+
+//   const theme = useTheme();
+//   const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
+
+//   useEffect(() => {
+//     if (anchorEl) {
+//       const rect = anchorEl.getBoundingClientRect();
+//       setPosition({
+//         top: rect.bottom + window.scrollY,
+//         left: rect.left + window.scrollX,
+//       });
+//     }
+//   }, [anchorEl]);
+
+//   const handleColorChange = (colorResult: any) => {
+//     setCurrentColor(colorResult.hex);
+//   };
+
+//   const addFavorite = () => {
+//     if (!favorites.includes(currentColor)) {
+//       setFavorites([...favorites, currentColor]);
+//     }
+//   };
+
+//   const handleFavoriteClick = (color: string) => {
+//     setCurrentColor(color);
+//     onClose(color);
+//   };
+
+//   return (
+//     <Modal open={open} onClose={() => onClose()}>
+//       <Box
+//         sx={{
+//           display: "flex",
+//           flexDirection: "column",
+//           alignItems: "center",
+//           justifyContent: "center",
+//           position: "absolute",
+//           top: `${position.top}px`,
+//           left: `${position.left}px`,
+//           bgcolor: "white",
+//           padding: 4,
+//           borderRadius: 2,
+//           width: fullScreen ? "90%" : 300,
+//         }}
+//       >
+//         <SketchPicker color={currentColor} onChange={handleColorChange} />
+//         <Button onClick={addFavorite} sx={{ marginTop: 2 }}>
+//           Add to Favorites
+//         </Button>
+//         <Box sx={{ marginTop: 2 }}>
+//           <Grid container spacing={1}>
+//             {favorites.map((color) => (
+//               <Grid item key={color}>
+//                 <Box
+//                   sx={{
+//                     width: 30,
+//                     height: 30,
+//                     bgcolor: color,
+//                     cursor: "pointer",
+//                   }}
+//                   onClick={() => handleFavoriteClick(color)}
+//                 />
+//               </Grid>
+//             ))}
+//           </Grid>
+//         </Box>
+//       </Box>
+//     </Modal>
+//   );
+// };
+
+// export default ColorPickerModal;
