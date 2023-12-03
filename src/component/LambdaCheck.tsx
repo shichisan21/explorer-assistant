441c3e060319ce89764b8d1a8ff6401f93245ee7
shichisan21 <photograph.by@gmail.com>
2023-12-04 02:28:51 +0900

    ランダムテキスト生成

--- src/component/LambdaCheck.tsx
+++ src/component/LambdaCheck.tsx
@@ -1,14 +1,36 @@
 import { Box, Button, Typography } from "@mui/material";
 import axios from "axios";
-import useState from "react";
+import React, { useState } from "react";
+
+interface LambdaText {
+  lambdaTest: string;
+  response: string;
+  result: string;
+}
 
 const LambdaCheck: React.FC = () => {
+  const [lambdaText, setLambdaText] = useState<LambdaText>();
+  const [error, setError] = useState(""); // エラーメッセージのためのステート
+
+  const handleGenerateText = (length: number) => {
+    const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let randomTextResult = "";
+    for (let i = 0; i < length; i++) {
+      randomTextResult += characters.charAt(
+        Math.floor(Math.random() * characters.length)
+      );
+    }
+    return randomTextResult;
+  };
+
   const postToLambda = async () => {
+    const newString = handleGenerateText(10); // 10文字のランダム文字列を生成
     try {
-      const response = await axios.post(
+      const response: any = await axios.post(
         "ENDPOINT",
         {
-          lambdaTest: "これはテスト",
+          lambdaTest: newString,
         },
         {
           headers: {
@@ -17,15 +39,22 @@ const LambdaCheck: React.FC = () => {
         }
       );
       console.log("Upload successful:", response.data);
+      setLambdaText(response.data);
     } catch (error) {
       console.error("Upload error:", error);
+      setError("アップロードに失敗しました。");
     }
   };
 
   return (
-    <Box>
-      <Button onClick={postToLambda}>PUSH TO POST</Button>
-    </Box>
+    <>
+      <Box>
+        <Button onClick={postToLambda}>PUSH TO POST</Button>
+      </Box>
+      <Typography variant='h5'>{lambdaText?.lambdaTest}</Typography>
+      {error && <Typography color='error'>{error}</Typography>}{" "}
+      {/* エラーメッセージの表示 */}
+    </>
   );
 };
 
