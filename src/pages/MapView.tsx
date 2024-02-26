51c0f54a7a80af9147ff50e91bb7b8c644230a38
shichisan21 <photograph.by@gmail.com>
2024-02-26 23:24:58 +0900

    Mapコンポーネント移動

--- src/pages/MapView.tsx
+++ src/pages/MapView.tsx
@@ -1,10 +1,115 @@
-import React, { useState, useRef, useEffect } from "react";
-import { Box, Input, Button, List, ListItem } from "@mui/material";
+/**
+ * File Description エントリーページ
+ */
+
+/**
+ * Import
+ */
+
+import "./App.css";
+import React, { useEffect, useRef, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+} from "react-router-dom";
+
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  Box,
+  TextField,
+  Button,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+
+import { Status, Wrapper } from "@googlemaps/react-wrapper";
+import { MapViewerComponent } from "../component/MapViewerComponent";
+import { Marker } from "../component/MapViewerComponent/Marker";
+
+type PositionStore = {
+  id: number;
+  lat: number;
+  lng: number;
+  positionName: string;
+};
 
 const MapView: React.FC = () => {
   const [message, setMessage] = useState<string>("");
   const [messages, setMessages] = useState<string[]>([]);
   const wsRef = useRef<WebSocket | null>(null);
+  // ログイン有効時間
+  const EXPIRY_TIME = 1000 * 60 * 60; // 1時間（ミリ秒単位）
+  const [loading, setLoading] = useState(true);
+
+  const checkIsLoggedIn = () => {
+    const loggedInTime = localStorage.getItem("loggedInTime");
+    const currentTime = new Date().getTime();
+    const isLoggedIn = localStorage.getItem("loggedIn");
+
+    // loggedInTimeがnullでないかどうかと、数値に変換できるかどうかをチェック
+    if (loggedInTime && !isNaN(Number(loggedInTime)) && isLoggedIn === "true") {
+      return currentTime - Number(loggedInTime) < EXPIRY_TIME;
+    }
+    return false;
+  };
+
+  // ステートの初期値をlocalStorageから取得する
+  const [loggedIn, setLoggedIn] = useState(checkIsLoggedIn());
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const [lastLoginTime, setLastLoginTime] = useState<string | null>(null);
+  const [position, setPosition] = useState<google.maps.LatLngLiteral>({
+    lat: 34.2422,
+    lng: 132.555,
+  });
+  const [positionStore, setPositionStore] = useState<PositionStore[]>([]);
+  const [markerPosition, setMarkerPosition] =
+    useState<google.maps.LatLngLiteral>();
+  const [lat, setLat] = useState<string>("");
+  const [lng, setLng] = useState<string>("");
+  const [positionName, setPositionName] = useState<string>("");
+
+  const render = (status: Status) => {
+    return <h1>{status}</h1>;
+  };
+
+  const setPositionValues = () => {
+    const latValue = position.lat;
+    const lngValue = position.lng;
+
+    // 緯度と経度が有効な数値であることを確認
+    if (!isNaN(latValue) && !isNaN(lngValue)) {
+      setPosition({ lat: latValue, lng: lngValue });
+      setPositionStore([
+        ...positionStore,
+        {
+          id: Date.now(),
+          lat: latValue,
+          lng: lngValue,
+          positionName: positionName,
+        },
+      ]);
+      setPositionName("");
+    } else {
+      // 無効な入力の場合のエラー処理（アラートなど）
+      alert("Invalid latitude or longitude");
+    }
+  };
 
   useEffect(() => {
     wsRef.current = new WebSocket("ws://localhost:6789");
@@ -36,43 +141,110 @@ const MapView: React.FC = () => {
       setMessage("");
     }
   };
+  const handleMapClick = (lat: any, lng: any) => {
+    console.log("クリック位置", lat, lng);
+    setMarkerPosition({ lat: lat, lng: lng });
+  };
+
+  const handleChangeLat = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLat(e.target.value);
+  };
+
+  const handleChangeLng = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLng(e.target.value);
+  };
+
+  const handleChangePositionName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPositionName(e.target.value);
+  };
+
+  const onCallHistoryItem = (lat: number, lng: number) => {
+    setPosition({ lat: lat, lng: lng });
+  };
+
+  const onDeleteHistoryItem = (id: number) => {
+    const filterdItem = positionStore.filter((item) => item.id != id);
+    setPositionStore(filterdItem);
+  };
 
   return (
-    <Box sx={{ margin: 2 }}>
-      <Input
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-        placeholder='Type a message...'
-      />
-      <Button
-        variant='contained'
-        sx={{
-          margin: 2,
-          backgroundColor: "green", // 緑色を背景として追加
-          "&:hover": {
-            backgroundColor: "darkgreen", // ホバー時の色も少し暗くする
-          },
-          "&:focus": {
-            // フォーカス時のスタイルを定義
-            outline: "none", // アウトラインを削除
-            boxShadow: "none", // シャドウを削除
-          },
-        }}
-        onClick={handleSendMessage}
-      >
-        Send
-      </Button>
-
-      <List>
-        {messages.map((msg, index) => (
-          <ListItem key={index}>{msg}</ListItem>
-        ))}
-      </List>
-
-      <Box sx={{ fontSize: "10px", marginTop: "20px" }}>
-        © {new Date().getFullYear()} All Rights Reserved
+    <>
+      <Box sx={{ minWidth: 500, marginBottom: 10 }}>
+        <Wrapper apiKey={import.meta.env.VITE_APP_MAP_API_KEY} render={render}>
+          <MapViewerComponent
+            style={{ width: "100%", aspectRatio: "4 / 3" }}
+            center={position}
+            onMapClick={handleMapClick}
+          >
+            <Marker
+              position={position}
+              setPosition={(lat, lng) => setPosition({ lat, lng })}
+            />
+          </MapViewerComponent>
+        </Wrapper>
+        <Typography>Latitude: {position.lat}</Typography>
+        <Typography>Longtitude: {position.lng}</Typography>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "Row",
+            alignItems: "center",
+            gap: 2,
+          }}
+        >
+          <Typography>Latitude: </Typography>
+          <TextField onChange={handleChangeLat} value={position.lat} />
+          <Typography>Longitude: </Typography>
+          <TextField onChange={handleChangeLng} value={position.lng} />
+          <Typography>Name: </Typography>
+          <TextField onChange={handleChangePositionName} value={positionName} />
+          <Button variant='contained' onClick={setPositionValues}>
+            SET
+          </Button>
+        </Box>
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>No</TableCell>
+                <TableCell align='right'>Name</TableCell>
+                <TableCell align='right'>Latitude</TableCell>
+                <TableCell align='right'>Longitude</TableCell>
+                <TableCell align='right'>Actions</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {positionStore.map((data, id) => (
+                <TableRow key={data.id}>
+                  <TableCell component='th' scope='row'>
+                    {id + 1}
+                  </TableCell>
+                  <TableCell align='right'>{data.positionName}</TableCell>
+                  <TableCell align='right'>{data.lat}</TableCell>
+                  <TableCell align='right'>{data.lng}</TableCell>
+                  <TableCell align='right'>
+                    <Button
+                      variant='contained'
+                      sx={{ margin: 1 }}
+                      onClick={() => onCallHistoryItem(data.lat, data.lng)}
+                    >
+                      復旧
+                    </Button>
+                    <Button
+                      variant='contained'
+                      sx={{ margin: 1 }}
+                      onClick={() => onDeleteHistoryItem(data.id)}
+                    >
+                      削除
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Box>
-    </Box>
+    </>
   );
 };
 
