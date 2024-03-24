891db2ab8ad07c1bc1943fde8d545983e9cdfd0e
shichisan21 <photograph.by@gmail.com>
2024-03-25 00:46:55 +0900

    supabase用

---
+++ src/component/SupabaseConnectTest.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+
+const TemplatePageComponent: React.FC = ({}) => {
+  return (
+    <Box>
+      <Typography>this is template</Typography>
+    </Box>
+  );
+};
+
+export default TemplatePageComponent;
