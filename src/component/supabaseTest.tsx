b1feceb54e4e863aa5eee050006579c377b911eb
shichisan21 <photograph.by@gmail.com>
2024-04-18 23:08:34 +0900

    API修正

--- src/component/supabaseTest.tsx
+++ src/component/supabaseTest.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Box, Typography } from "@mui/material";
+import axios, { AxiosResponse } from "axios";
 
-const SupabaseTest: React.FC = ({}) => {
+const SupabaseTest = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response: AxiosResponse<any> = await axios.get("/api/hello");
+        console.log("GET successful:", response.data);
+      } catch (error) {
+        console.error("GET Error:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <Box>
       <Typography>this is template</Typography>
--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,5 +1,5 @@
-const handler = (req: any, res: any) => {
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
   res.status(200).json({ name: "John Doe" });
 };
-
-export default handler;
